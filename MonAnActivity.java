ed28bcb757dbee92c737f38ebc3100f72f1a2b26
vuducbang <136888800+vuducbang@users.noreply.github.com>
2023-10-04 03:14:51 +0700

    Add files via upload

---
+++ MonAnActivity.java
@@ -0,0 +1,12 @@
+package com.example.baitaplon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonAnActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_an);
+    }
+}
