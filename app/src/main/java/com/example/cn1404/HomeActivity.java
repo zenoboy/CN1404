29ea98aad5e63861bb45fa356e714c096bf5bc62
Dũng <95403173+zenoboy@users.noreply.github.com>
2023-10-11 03:04:44 +0700

    11/10/2023

--- app/src/main/java/com/example/cn1404/HomeActivity.java
+++ app/src/main/java/com/example/cn1404/HomeActivity.java
@@ -1,60 +1,71 @@
 package com.example.cn1404;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;;
 
 
 public class HomeActivity extends AppCompatActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home);
-//        // Khai báo Toolbar và kết nối nó với ID đã đặt trong tệp XML
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setTitle("Tiêu đề của ActionBar");
-//
-//
-//    };
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home);
-//
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setTitle("Tiêu đề của ActionBar");
-//
-//    }
-  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
 
-//        // Tìm kiếm
-//        MenuItem searchItem = menu.findItem(R.id.action_search);
-//        SearchView searchView = (SearchView) searchItem.getActionView();
-//
-//        // Thiết lập lắng nghe cho sự kiện tìm kiếm
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                // Xử lý khi người dùng nhấn nút tìm kiếm
-//                return true;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                // Xử lý khi người dùng thay đổi văn bản trong ô tìm kiếm
-//                return true;
-//            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Kích hoạt chức năng tìm kiếm
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Xử lý khi người dùng nhấn nút tìm kiếm
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Xử lý khi người dùng thay đổi văn bản tìm kiếm
+                return true;
+            }
+        });
 
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            // Xử lý khi biểu tượng tìm kiếm được nhấn
+            return true;
+        }
+        if (id == R.id.action_keypad) {
+            // Xử lý khi biểu tượng "keypad" được nhấn
+            return true;
+        }
+
+        // Xử lý các mục menu khác nếu cần
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+
+
 
 
 }
