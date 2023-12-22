7476a85f9edfe84106fd1008910b979458e5bdcd
ductrungD <ducvnmw@gmail.com>
2023-12-22 18:58:25 +0700

    thêm các layout món ăn

---
+++ app/src/main/java/com/example/cn1404/Do_Uong_Pha_CheActivity.java
@@ -0,0 +1,14 @@
+package com.example.cn1404;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Do_Uong_Pha_CheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_uong_pha_che);
+    }
+}
